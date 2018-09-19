33420b21de505fc9b186a52564c87c5e42d2256f
Hjalte Daniel Hansen <hjal0179@edu.eal.dk>
2018-09-19 17:19:07 +0200

    Initial commit

---
+++ StoryTelling/Objects/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTelling.Objects
+{
+    class Person
+    {
+        public string Name;
+        public int YearOfBirth;
+        public string CityOfBirth;
+        public string MothersName;
+        public string FathersName;
+
+        public string LifeStory()
+        {
+            string storyTime = "Well. Let me tell you your life story " + Name + ". You were born way back in " + YearOfBirth + " in the city of " + CityOfBirth + " your father, the great man was " + FathersName + " and his lovely and caring wife, your mother, " + MothersName + ". Now you know a little more about yourself, don't you?";
+            return storyTime;
+        }
+    }
+}
---
+++ StoryTelling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoryTelling.Objects;
+
+namespace StoryTelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Storytime app, by the world famous company.");
+            Console.WriteLine("Let me start to know your name:");
+
+            Person individualPerson = new Person();
+            individualPerson.Name = Console.ReadLine();
+
+            Console.WriteLine("That is a nice name. When were you born?");
+            individualPerson.YearOfBirth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("And who is your father?");
+            individualPerson.FathersName = Console.ReadLine();
+
+            Console.WriteLine("And your lovely and caring mother is?");
+            individualPerson.MothersName = Console.ReadLine();
+
+            Console.WriteLine("And in what city did you welcome this world?");
+            individualPerson.CityOfBirth = Console.ReadLine();
+
+            Console.WriteLine(individualPerson.LifeStory());
+
+            Console.Read();
+        }
+    }
+}
